75eeaa710cf8c1bcf2c233e7617acc3caf4846a1
senpaiblue <singhsakcham29@email.com>
2024-03-04 22:50:59 +0530

    folder-setup

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View className="pt-20 bg-red-200">
+      <Text>Home</Text>
+    </View>
+  )
+}
